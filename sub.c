3919248c7c58fa335fe755a0ce82ec77bdc1cfb2
ummecode <ummeh350@gmail.com>
2022-04-24 11:27:14 +0530

    changes

--- sub.c
+++ sub.c
@@ -2,10 +2,10 @@
 int main()
 {
 int a,b,c;
-printf("Enter two numbers-")
+printf("Enter two numbers-");
 scanf("%d",&a);
 scanf("%d",&b);
 c=a-b;
-printf("The difference is %d",c);
+printf("The difference is %d\n",c);
 return 0;
 }
